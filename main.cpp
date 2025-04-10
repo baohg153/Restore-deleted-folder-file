1017118949bbc0264eea2f5c5b1c9dc8ee561f9d
baohg153 <huynhgiabao27112005@gmail.com>
2025-04-10 22:54:20 +0700

    Add some color for FAT32

--- main.cpp
+++ main.cpp
@@ -93,19 +93,21 @@ void doFAT(char volumeChar)
     vector<ITEM> folders, deletedFiles;
     int recentSector = firstDataSector;
     stack<int> prevSectors;
+    string currentPath(1, volumeChar); currentPath = currentPath + ":/"; 
     while (1)
     {
         system("cls");
+        std::cout << "\033[1;34mPATH: \033[34m" << currentPath << "\033[0m\n-------------------------------------------------------\n";
         getCertainFolder(hVolume, recentSector, folders, deletedFiles);
 
         //print folders and deleted files
-        cout << "FOLDERS: \n0>..\n";
-        for (int i = 1; i < folders.size(); i++)
+        cout << "\033[1;33mFOLDERS:\033[0m \n0>..\n";
+        for (int i = 1; i< folders.size(); i++)
             cout << i << "> " << get<1>(folders[i]) << "\n";
-        cout << "\nDELETED FILES: \n";
+        cout << "\n\033[1;33mDELETED FILES:\033[0m\n";
         for (int i = 0; i < deletedFiles.size(); i++)
             cout << i << "> " << get<1>(deletedFiles[i]) << "\n";
-        cout << "\n Commands: \"cd [num]\" or \"restore [num]\" or \"quit\" or \"reset\"\n> ";
+        cout << "\n \033[0;32m Commands: \"cd [num]\" or \"restore [num]\" or \"quit\" or \"reset\" \n> \033[0m";
 
         //Type in command
         string cmd; int num;
@@ -128,25 +130,36 @@ void doFAT(char volumeChar)
 
             continue;
         }
-        cin >> num;
 
         //Process command
-        if (cmd == "cd" && (num == 0 || (num >= 0 && num < folders.size())))
+        if (cmd == "cd")
         {
+            cin >> num;
+            if (!(num == 0 || (num >= 0 && num < folders.size()))) continue;
             if (num == 0)
             {
                 if (prevSectors.empty()) continue;
 
                 recentSector = prevSectors.top();
                 prevSectors.pop();
+                
+                //change currentPath
+                int pos = currentPath.size() - 2;
+                while (currentPath[pos--] != '/');
+                currentPath.erase(pos + 2);
+
                 continue;
             }
 
             prevSectors.push(recentSector);
             recentSector = firstDataSector + (get<4>(folders[num]) - 2)*4;
+            currentPath = currentPath + get<1>(folders[num]) + "/";
         }
-        else if (cmd == "restore" && num >= 0 && num <= deletedFiles.size() && !deletedFiles.empty())
+        else if (cmd == "restore")
         {
+            cin >> num;
+            if (!num >= 0 && num <= deletedFiles.size() && !deletedFiles.empty()) continue;
+
             int retval = restoreItem(hVolume, deletedFiles[num]);
             if (retval == -1) cout << "Could not restore all data of the file!\n";
             else if (retval == 0) cout << "Restored successfully!\n";
@@ -162,14 +175,13 @@ void doFAT(char volumeChar)
                 FILE_FLAG_NO_BUFFERING, 
                 NULL
             );
-
             system("pause");
         }   
     }
 
     CloseHandle(hVolume);
     return;
-}   
+}    
 
 int main()
 {
